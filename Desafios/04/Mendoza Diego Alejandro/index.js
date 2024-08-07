032ed80224ce6ad0c9ac824dccbd05d198093de5
Emanuel Some <em7som@gmail.com>
2024-08-06 23:43:20 -0300

    Correccion - Terminado 04 mendoza y oliveira

--- Desafios/04/Mendoza Diego Alejandro/index.js
+++ Desafios/04/Mendoza Diego Alejandro/index.js
@@ -1,13 +1,5 @@
 
 
-var login = prompt("Ingrese su login:");
-var password = prompt("Ingrese su password:");
 
-if (login === "ale" && password === "mendoza") {
-  alert("Bienvenido, administrador!");
-} else {
-  alert("Login o password incorrectos. Por favor, inténtelo nuevamente.");
-  window.location.href = "index.html"; // Redirige a la página principal
-}
 
 
