8dc8c3ceae8551e869158826c7eeb570eea4d099
Ema_Ubuntu_Work <em7som@gmail.com>
2024-09-25 16:12:19 -0300

    CORRECCION - Termine Matias Villalba

--- Desafios/07/Villalba Matias/JS/index.js
+++ Desafios/07/Villalba Matias/JS/index.js
@@ -25,11 +25,13 @@ miBoton.addEventListener("click", (e) => {
 
     switch (user) {
         case "mario":
+            ocultarTodos()
             span.textContent = user;
             
             div.setAttribute("title", "Presentado");
             break;
         case "daisy":
+            ocultarTodos()
             span.innerHTML = `Princesa ${user}`;
             div.setAttribute("title", "Presentado");
             break;
@@ -38,40 +40,55 @@ miBoton.addEventListener("click", (e) => {
                 div.setAttribute("title", "Presentado");
             break;
         case "luigi":
+            ocultarTodos()
             span.innerHTML = user;
             div.setAttribute("title", "Presentado");
             break;
         case "bowser":
+            ocultarTodos()
             span.innerHTML = `Bowser Morton Koopa`;
             div.setAttribute("title", "Presentado");
             break;
         case "peach":
+            ocultarTodos()
             span.textContent  = `Princesa Peach Toadstool`;
             div.setAttribute("title", "Presentado");
             break;
         case "toad":
+            ocultarTodos()
             span.innerHTML = user;
             div.setAttribute("title", "Presentado");
             break;
         case "toadette":
+            ocultarTodos()
             span.innerHTML = user;
             div.setAttribute("title", "Presentado");
             break;
        
         default:
-            span.innerHTML = "Desconocido";
+            /* span.innerHTML = "Desconocido"; */
             break;
     } 
-
-
 });
 
+const ocultarTodos = () => {
+    let divPersonajes = document.querySelectorAll('#cajas div')
+    console.log(divPersonajes)
+    divPersonajes.forEach(div => {
+        div.removeAttribute("title")
+    });
+}
 
 const mostrar = (user) => {
-
-    let div = document.getElementById(`${user}`);
-    let span = document.getElementById("personaje");
-    span.textContent = user;
-    div.setAttribute("title", "Presentado");
     
+    let div = document.getElementById(`${user}`);
+        
+        if(div.getAttribute('title') === 'Presentado'){
+            div.removeAttribute('title')
+        }else{
+            ocultarTodos()
+            let span = document.getElementById("personaje");
+            span.textContent = user;
+            div.setAttribute("title", "Presentado");
+        }
 }
